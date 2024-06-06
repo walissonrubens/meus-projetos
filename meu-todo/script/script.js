e4ce6277f5b861979ec52aad83e71c5a547bbba4
Walisson <143977212+walissonrubens@users.noreply.github.com>
2024-06-06 18:24:45 -0300

    finalizei javascript

--- meu-todo/script/script.js
+++ meu-todo/script/script.js
@@ -1,14 +1,13 @@
 let addBtn = document.querySelector("#add_button")
 let input = document.querySelector("#input")
-let todoContainer = document.querySelector(".todo-container") 
+let todoContainer = document.querySelector(".todo-container")
 
-// Evento para adicionar tarefa
+// Evento para adicionar tarefa clique
+addBtn.addEventListener("click", () => {
 
-addBtn.addEventListener("click", () =>{
-
-    if(input.value == ""){
-        return alert("Digite algo")
-     }
+    if (input.value == "") {
+        return alert("Digite algo") 
+    }
 
     let todoDiv = document.createElement("div")
     todoDiv.classList.add("todo")
@@ -18,11 +17,12 @@ addBtn.addEventListener("click", () =>{
     todoDiv.appendChild(titleDiv)
     let todoText = document.createElement("h3")
     titleDiv.appendChild(todoText)
+    titleDiv.classList.add("todo-title")
 
     let btnDiv = document.createElement("div")
     btnDiv.classList.add("buttons")
-     todoDiv.appendChild(btnDiv)
-    
+    todoDiv.appendChild(btnDiv)
+
 
     todoText.innerHTML = input.value
     input.value = ""
@@ -30,9 +30,41 @@ addBtn.addEventListener("click", () =>{
     buttons(btnDiv, todoDiv, todoContainer)
 })
 
+// Evento para adiconar tarefa tecla enter
+
+input.addEventListener("keydown", (e) => {
+
+    if (e.keyCode == 13) {
+        if (input.value == "") {
+            return alert("Digite algo")
+        }
+
+        let todoDiv = document.createElement("div")
+        todoDiv.classList.add("todo")
+        todoContainer.appendChild(todoDiv)
+
+        let titleDiv = document.createElement("div")
+        todoDiv.appendChild(titleDiv)
+        let todoText = document.createElement("h3")
+        titleDiv.appendChild(todoText)
+        titleDiv.classList.add("todo-title")
+
+        let btnDiv = document.createElement("div")
+        btnDiv.classList.add("buttons")
+        todoDiv.appendChild(btnDiv)
+
+
+        todoText.innerHTML = input.value
+        input.value = ""
+
+        buttons(btnDiv, todoDiv, todoContainer)
+    }
+})
+
 // Funções
 
-function buttons(btnContainer, todoDiv, container){
+function buttons(btnContainer, todoDiv, container) {
+    // Criar botões
     let btnCheck = document.createElement("button")
     btnCheck.innerHTML = '<i class="bi bi-check-circle"></i>'
     btnContainer.appendChild(btnCheck)
@@ -45,19 +77,56 @@ function buttons(btnContainer, todoDiv, container){
     btnDelete.innerHTML = '<i class="bi bi-trash3"></i>'
     btnContainer.appendChild(btnDelete)
 
-    btnCheck.addEventListener("click", () =>{
+    // Evento de confirmar tarefa
+    btnCheck.addEventListener("click", () => {
         todoDiv.classList.toggle("check")
         btnContainer.classList.toggle("checkBtn")
     })
 
-    btnEdit.addEventListener("click", ()=>{
+    btnDelete.addEventListener("click", (e)=>{
+        let targetEl = e.currentTarget.closest(".todo")
+        targetEl.remove()
+    })
+
+    // Evento do botão de edição
+    btnEdit.addEventListener("click", (e) => {
         container.classList.add("hide")
 
-        let editInput = document.createElement("input")
-        editInput.placeholder = "Edite sua tarefa"
+        let targetEl = e.currentTarget.closest(".todo")
+        let targetText = targetEl.querySelector(".todo-title h3")
 
-        let divInput = document.createElement("div")
-        document.querySelector(".container").appendChild(divInput)
-        divInput.appendChild(editInput)
+        let divContainer = document.createElement("div")
+        divContainer.classList.add("container")
+        document.querySelector(".container").appendChild(divContainer)
+
+        let editTitle = document.createElement("h3")
+        editTitle.innerText = "Edite sua tarefa"
+        divContainer.appendChild(editTitle)
+
+        let editInput = document.createElement("input")
+        editInput.value = targetText.textContent
+        divContainer.appendChild(editInput)
+
+        let editBtnConfirm = document.createElement("button")
+        editBtnConfirm.innerHTML = '<i class="bi bi-check-circle"></i>'
+        divContainer.appendChild(editBtnConfirm)
+
+        // Evento do botão para confirmar edição
+        editBtnConfirm.addEventListener("click", () => {
+            container.classList.remove("hide")
+            targetText.textContent = editInput.value
+            divContainer.classList.add("hide");
+        })
+
+        // Evento para confirmar com tecla enter
+
+        editInput.addEventListener("keydown", (e) => {
+            if (e.key === "Enter") {
+                container.classList.remove("hide")
+                targetText.textContent = editInput.value
+                divContainer.classList.add("hide");
+            }
+        })
     })
+    
 }
